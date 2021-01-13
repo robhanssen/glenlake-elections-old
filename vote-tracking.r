bb04b3921ba4397cd7a2e0053745aa6d97c95c79
Rob Hanssen <rob.hanssen@outlook.com>
2021-01-13 13:29:27 -0500

    update graph and code with constants

--- vote-tracking.r
+++ vote-tracking.r
@@ -1,6 +1,17 @@
+# default libraries
 library(tidyverse)
 library(lubridate)
 
+#define constants
+MEETINGDATE = as.Date("2021-02-06", format="%Y-%m-%d")
+QUORUM = 120
+YMAX_DEFAULT = 160
+SCALING = YMAX_DEFAULT / QUORUM
+YLABELS = seq(0,500,30)
+XLIMITS = c(as.Date("2021-01-11"),as.Date("2021-02-10"))
+
+
+# read data file
 votes <- read_csv("vote-tracking.csv") %>% mutate(date = as.Date(date, format="%Y-%m-%d"))
 
 # caption generator
@@ -9,20 +20,29 @@ lastupdate = format(max(votes$date), format="%b %d, %Y")
 capt = paste0("\U00A9 2021, Glenlake Homeowners Association\nLast updated ", lastgen, "\nLast data entry: ", lastupdate)
 
 # y-axis max
-ymax = ifelse(max(votes$votesreceived)<120, 160, ((max(votes$votesreceived)*1.33)%/%10 + 1)*10 )
+votesmax = max(votes$votesreceived)
+ymax = ifelse(votesmax < QUORUM, 
+                    YMAX_DEFAULT, 
+                    ((votesmax * SCALING) %/% 10 + 1) * 10
+                    )
+
+#define YLIMIT constants
+YLIMITS = c(0 , ymax)
 
 
 votes %>% ggplot + aes(x=date, y=votesreceived) + 
             geom_point() + 
             geom_smooth(method="lm", lty=2, color="gray") + 
-            scale_x_date(date_breaks="1 week", date_labels = "%b %d", limit=c(as.Date("2021-01-11"),as.Date("2021-02-10"))) + 
-            scale_y_continuous(limit=c(0,ymax), breaks=seq(0,500,30)) + 
+            scale_x_date(date_breaks="1 week", date_labels = "%b %d", limit=XLIMITS) + 
+            scale_y_continuous(limit = YLIMITS, breaks = YLABELS, 
+                               sec.axis = sec_axis(~ ./QUORUM*100, breaks=seq(0,100,25), name="Percentage of quorum")
+                                ) + 
             labs(x="Date", y="Votes received", caption=capt)  +
             ggtitle("Votes received for the 2021 Glen Lake Board of Directors election") +
             theme_light() + 
-            geom_hline(yintercept=120, lty=2) + 
-            geom_vline(xintercept=as.Date("2021-02-06"), lty=2, color="red") + 
-            annotate("text",x=as.Date("2021-01-12", format="%Y-%m-%d"),y=125,label="Quorum") + 
-            annotate("text",x=as.Date("2021-02-05", format="%Y-%m-%d"),y=60,label="Annual Meeting date", angle=90)
+            geom_hline(yintercept = QUORUM, lty=2, color="red") + 
+            geom_vline(xintercept = MEETINGDATE, lty = 2, color = "red") + 
+            annotate("text",x = as.Date("2021-01-13", format="%Y-%m-%d"), y = 125, label = paste0("Quorum: ", QUORUM)) + 
+            annotate("text",x = as.Date("2021-02-05", format="%Y-%m-%d"), y = 60, label = "Annual Meeting date", angle = 90)
 
 ggsave("graphs/vote-tracking.png")
