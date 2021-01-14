d9d1d2ce8fe214e90c850d62fbac3844c727d615
Rob Hanssen <rob.hanssen@outlook.com>
2021-01-14 08:53:40 -0500

    added 2020 data

--- vote-tracking-2020.r
+++ vote-tracking-2020.r
@@ -5,17 +5,17 @@ library(ggrepel)
 options(bitmapType='cairo')
 
 #define constants
-MEETINGDATE = as.Date("2021-02-06", format="%Y-%m-%d")
+MEETINGDATE = as.Date("2020-02-08", format="%Y-%m-%d")
 QUORUM = 120
 YMAX_DEFAULT = 160
 SCALING = YMAX_DEFAULT / QUORUM
 YLABELS = seq(0,500,30)
-XLIMITS = c(as.Date("2021-01-06"),as.Date("2021-02-10"))
+XLIMITS = c(as.Date("2020-01-15"),as.Date("2020-02-13"))
 PERCENTBREAKS = seq(0,4*QUORUM,25)
-YEAR = 2021
+YEAR = 2020
 
 # read data file
-votes <- read_csv("sources/vote-tracking-2021.csv") %>% 
+votes <- read_csv("sources/vote-tracking-2020.csv") %>% 
                     mutate(date = as.Date(date, format="%Y-%m-%d"),
                            pastquorum = ifelse(votesreceived < QUORUM, FALSE, TRUE)
                             )
@@ -48,10 +48,10 @@ votes %>% ggplot + aes(x=date, y=votesreceived, label=votesreceived) +
             geom_hline(yintercept = QUORUM, lty=2, color="red") + 
             geom_vline(xintercept = MEETINGDATE, lty = 2, color = "red") + 
             geom_label_repel(aes(date,votesreceived, label=votesreceived, fill=pastquorum), color="white") +             
-            annotate("text",x = as.Date("2021-01-13", format="%Y-%m-%d"), y = 125, label = paste0("Quorum: ", QUORUM)) + 
-            annotate("text",x = as.Date("2021-02-05", format="%Y-%m-%d"), y = 60, label = "Annual Meeting", angle = 90) + 
+            annotate("text",x = as.Date("2020-01-13", format="%Y-%m-%d"), y = 125, label = paste0("Quorum: ", QUORUM)) + 
+            annotate("text",x = as.Date("2020-02-07", format="%Y-%m-%d"), y = 60, label = "Annual Meeting", angle = 90) + 
             theme_light() + 
             theme(legend.position = "none")
 
-ggsave("graphs/vote-tracking-2021.png")
-ggsave("graphs/vote-tracking-2021.pdf")
+ggsave("graphs/vote-tracking-2020.png")
+ggsave("graphs/vote-tracking-2020.pdf")
